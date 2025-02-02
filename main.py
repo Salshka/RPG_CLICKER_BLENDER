28320b6215c5d769ac2c1592cac40e460132529e
Salshka <Julienbrouzes@hotmail.com>
2025-02-02 16:49:47 +0100

    better draws

--- main.py
+++ main.py
@@ -66,7 +66,6 @@ base_dir = os.path.dirname(bpy.data.filepath) #Path for everything
 
 #INVENTORY
 inventory = [None] * 10
-custom_icons = None
 dice = (1 ,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 launch = None
 
